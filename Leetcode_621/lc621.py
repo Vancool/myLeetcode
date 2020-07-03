1f1d147abcdeacd6a8b27523ee7483c1e9d04b50
vanco <vancoLynn@yeah.net>
2020-07-03 11:09:24 +0800

    2020/7/3 leetcode test

---
+++ Leetcode_621/lc621.py
@@ -0,0 +1,25 @@
+import heapq
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        t = 0
+        hashmap = {}
+        for task in tasks:
+            hashmap[task] = hashmap.get(task, 0)+1
+        min_heap = []
+        for key, value in hashmap.items():
+            min_heap.append([0, key, value])
+        while min_heap:
+            heapq.heapify(min_heap)
+            time, task, count = heapq.heappop(min_heap)
+            t += time+1
+            for i in range(len(min_heap)):
+                min_heap[i][0] = 0 if min_heap[i][0] <= time+1 else min_heap[i][0] - time -1
+            count -= 1
+            if count > 0:
+                heapq.heappush(min_heap, [n, task, count])
+        return t
+
+a = Solution()
+tasks = ["A","A","A","B","B","B"]
+n = 2
+print(a.leastInterval(tasks, n))
